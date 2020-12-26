55e483acf984ccd2e0dda570eef0c54f95e27dca
Javier Soler Artero <javier.soler.jsa@gmail.com>
2020-12-26 13:47:52 +0100

    Settings DONE

--- src/pages/Profile/Settings.tsx
+++ src/pages/Profile/Settings.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { IonPage } from "@ionic/react";
+import { IonContent, IonItem, IonLabel, IonPage, IonToggle } from "@ionic/react";
 
 const Settings: React.FC = () => (
   <IonPage>
-    <p>Settings</p>
+    <IonContent>
+      <IonItem>
+        <IonLabel>Notificaciones Partidas</IonLabel>
+        <IonToggle checked color="primary" value="match_notifications" />
+      </IonItem>
+
+      <IonItem>
+        <IonLabel>Notificaciones Usuarios</IonLabel>
+        <IonToggle checked color="primary" value="users_notifications" />
+      </IonItem>
+    </IonContent>
   </IonPage>
 );
 
