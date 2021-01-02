07f983bcd140250d39501799181ff9491f04c512
Javier Soler Artero <javier.soler.jsa@gmail.com>
2021-01-02 13:58:04 +0100

    Card DONE

--- src/pages/Home/MatchCard.tsx
+++ src/pages/Home/MatchCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IonIcon, IonPage } from "@ionic/react";
-import { basketball } from "ionicons/icons";
+import { basketball, logoEuro, person, time } from "ionicons/icons";
 import "./MatchCard.css";
 
 const MatchCard: React.FC = () => (
@@ -18,7 +18,45 @@ const MatchCard: React.FC = () => (
     </div>
     <div className="halfCardBot">
       <div className="halfCardBotInside">
-        <h2>Hola</h2>
+        <div>
+          <h2>Ontinyent</h2>
+          <h3>Partido de futbol</h3>
+        </div>
+        <div className="infoContainer">
+          <div className="info">
+            <IonIcon
+              style={{
+                marginRight: "0.7rem",
+                color: "white",
+                fontSize: "2.3rem",
+              }}
+              icon={time}
+            />
+            <p>04:10</p>
+          </div>
+          <div className="info">
+            <IonIcon
+              style={{
+                marginRight: "0.7rem",
+                color: "white",
+                fontSize: "2.3rem",
+              }}
+              icon={person}
+            />
+            <p>3</p>
+          </div>
+          <div className="info">
+            <IonIcon
+              style={{
+                marginRight: "0.7rem",
+                color: "white",
+                fontSize: "2.3rem",
+              }}
+              icon={logoEuro}
+            />
+            <p>4</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
